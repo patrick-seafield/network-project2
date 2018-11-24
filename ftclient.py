7e72a5f3609a2958cba460f4c9c9462952fb7cbf
Patrick Seafield <seafielp@oregonstate.edu>
2018-11-24 01:04:12 -0800

    ftclient is receiving bytes from ftserver.

--- ftclient.py
+++ ftclient.py
@@ -7,11 +7,25 @@ import socket
 import sys
 from validateArgs import validate
 
+
+BUFFER_SIZE = 2048
+
 # main()
 if __name__ == '__main__':
+    # Check that the user has supplied valid arguments.
     opts = validate(sys.argv)
     if (opts is None):
         sys.exit(1)
-    
-    if opts['command'] == '-l':
-        print('Receiving directory structure from {serverHost}:{serverPort}'.format(**opts))
+
+    # Open the socket on the user-specified server.
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+        # Ask for a list of downloadable files.
+        if opts['command'] == '-l':
+            print('Receiving directory structure from {serverHost}:{serverPort}'.format(**opts))
+            s.connect((opts['serverHost'], opts['serverPort']))
+            # s.sendall(b'-l')
+
+            data = s.recv(BUFFER_SIZE)
+            if data is not None:
+                print(repr(data))
